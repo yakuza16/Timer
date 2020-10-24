bda51797ef2038e277c385dc28931c2a67dfe3ab
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2020-10-24 19:28:44 +0200

    feat working timer

--- scripts/Timer.js
+++ scripts/Timer.js
@@ -13,8 +13,10 @@ const {hoursInputId,minutesInputId,secondsInputId,editBtnId,playStopBtnId,refres
 let isEditable = true;
 let isTimerOn = false;
 const MAX_HOURS_VALUE = 99;
-const MAX_MINUTES_VALUE = 60;
-const MAX_SECONDS_VALUE = 60;
+const MAX_MINUTES_VALUE = 59;
+const MAX_SECONDS_VALUE = 59;
+
+let interval;
 
 class Timer {
 constructor(){
@@ -25,6 +27,10 @@ this.optionEdit = null;
 this.optionPlay = null;
 this.optionRefresh = null;
 this.alarm = null;
+
+this.choosenHours = null;
+this.choosenMinutes = null;
+this.choosenSeconds = null;
 
 }
 
@@ -47,6 +53,7 @@ bindToElements(){
 addListeners(){
 this.optionEdit.addEventListener('click',()=> this.setTimer());
 this.optionPlay.addEventListener('click', ()=>this.startTimer());
+this.optionRefresh.addEventListener('click', ()=>this.refreshTimer())
 }
 
 setTimer(){
@@ -65,8 +72,43 @@ setTimer(){
 startTimer(){
     this.optionPlay.classList.toggle('button--is-stop');
     isTimerOn = !isTimerOn;
-  
+
+    if(isTimerOn === true){
+        this.count()
+    } else if(isTimerOn === false){
+        clearInterval(interval)
+    }
 }
+
+count(){
+    const indexInterval = setInterval(()=>{
+    this.displaySeconds.value --;
+    this.displaySeconds.value < 10
+    ? this.displaySeconds.value = `0${this.displaySeconds.value}`
+    : this.displaySeconds.value;
+
+    if(this.displaySeconds.value <= 0){
+        this.displayMinutes.value --;
+        this.displayMinutes.value < 10
+        ? this.displayMinutes.value = `0${this.displayMinutes.value}`
+        :
+        this.displayMinutes.value;
+        this.displaySeconds.value = MAX_SECONDS_VALUE;
+    }
+
+    if(this.displayMinutes.value <= 0){
+        this.displayHours.value --;
+        this.displayHours.value < 10
+        ? this.displayHours.value = `0${this.displayHours.value}`
+        : this.displayHours.value;
+        this.displayMinutes.value = MAX_MINUTES_VALUE;
+    }
+  
+    },1000);
+    interval = indexInterval;
+}
+
+
 
 onOffInputs(isEditable){
     const inputs = [this.displayHours,this.displayMinutes,this.displaySeconds];
@@ -82,15 +124,32 @@ if(isEditable === true){
 checkValues(){
 this.displayHours.value > MAX_HOURS_VALUE
 ? this.displayHours.value = MAX_HOURS_VALUE
+: this.displayHours.value < 10 
+? this.displayHours.value = `0${this.displayHours.value}`
 : this.displayHours.value;
 
 this.displayMinutes.value > MAX_MINUTES_VALUE
-? this.displayMinutes.value = MAX_MINUTES_VALUE
+? this.displayMinutes.value = MAX_MINUTES_VALUE +1
+: this.displayMinutes.value < 10
+? this.displayMinutes.value = `0${this.displayMinutes.value}`
 : this.displayMinutes.value;
 
 this.displaySeconds.value > MAX_SECONDS_VALUE
-? this.displaySeconds.value = MAX_SECONDS_VALUE
+? this.displaySeconds.value = MAX_SECONDS_VALUE +1
+: this.displaySeconds.value < 10
+? this.displaySeconds.value = `0${this.displaySeconds.value}`
 : this.displaySeconds.value;
+
+
+this.choosenHours = this.displayHours.value;
+this.choosenMinutes = this.displayMinutes.value;
+this.choosenSeconds = this.displaySeconds.value;
+}
+
+refreshTimer(){
+ this.displayHours.value = this.choosenHours ;
+this.displayMinutes.value = this.choosenMinutes;
+this.displaySeconds.value = this.choosenSeconds;
 }
 
 toggleEditIcon(){
